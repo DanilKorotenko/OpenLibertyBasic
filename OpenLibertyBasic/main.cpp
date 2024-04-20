d2b92c903916bbac69ea71ff64caf4cf34a5772d
Danil Korotenko <danil.korotenko@gmail.com>
2024-04-20 18:17:04 +0300

    Initial Commit

---
+++ OpenLibertyBasic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenLibertyBasic
+//
+//  Created by Danil Korotenko on 4/20/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
